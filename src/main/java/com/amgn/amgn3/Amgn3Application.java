e4ef83faacab2655d4595a8ba62a1715e49ab599
sanghyeok07 <3400jkh@example.com>
2025-09-30 17:25:18 +0900

    Initial commit

---
+++ src/main/java/com/amgn/amgn3/Amgn3Application.java
@@ -0,0 +1,13 @@
+package com.amgn.amgn3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Amgn3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Amgn3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/amgn/amgn3/Amgn3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amgn.amgn3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Amgn3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
